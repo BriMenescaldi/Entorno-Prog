b810d0af14c4320f586e9d68a98c3dd68304c36a
Brisa <119879839+BriMenescaldi@users.noreply.github.com>
2022-12-06 07:49:37 -0300

    Create statsWords.sh

---
+++ statsWords.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+larga=0
+corta=189819 #cantidad de palabras de la palabra más larga que existe
+promedio=0
+suma=0 
+cantidad=0
+longitud_palabra=0 
+
+for word in $(cat $1);
+  do 
+    longitud_palabra=${#word} #para guardar la longitud de la palabra 
+    if [[ $longitud_palabra -lt  $corta ]
+    then 
+    corta=$longitud_palabra
+    fi
+    if [[ $longitud_palabra -gt $larga ]]
+    then
+    larga=$longitud_palabra
+    fi
+    suma=$((suma+longitud_palabra))
+    cantidad=$((cantidad+1))
+done
+
+promedio=$((suma/cantidad))
+
+echo "La palabra más larga tiene "$larga "letras."
+echo "La palabra más corta tiene "$corta "letras."
+echo "En promedio las palabras tienen "$promedio "letras."
+
+
+  
